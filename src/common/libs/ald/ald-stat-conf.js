edaebf2266e7d808c6c3f369a8421533e4810ae7
Hope <hopefullstack@gmail.com>
2018-04-06 21:49:50 +0800

    add: 增加统计

---
+++ src/common/libs/ald/ald-stat-conf.js
@@ -0,0 +1,5 @@
+exports.app_key = "c16874c82490b5b75a959c00dd8e612e"; //请在此行填写从阿拉丁后台获取的appkey
+exports.getLocation = true; //默认不获取用户坐标位置
+exports.appid = "wx2a3d7e73ca459b21"; //用于用户登录、微信转发群信息、二维码等微信官方功能
+exports.appsecret = "8152dbb58fd9a39df805ec3d2757c434";//用于用户登录、微信转发群信息、二维码等微信官方功能
+exports.defaultPath = 'pages/my/index';//小程序的默认首页, 用于分享时path为空时
